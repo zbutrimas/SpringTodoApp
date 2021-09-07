e42a951e85334f171b6deffeed76fc3966efceb3
Zydrunas <zbutrimas@gmail.com>
2021-09-07 10:28:17 +0300

    delete works

--- src/main/java/com/example/springtodoapp/controller/TodoAppController.java
+++ src/main/java/com/example/springtodoapp/controller/TodoAppController.java
@@ -10,8 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Map;
-
 @RestController
 @RequestMapping("/main")
 //@CrossOrigin(origins = "*", maxAge = 3600) //tinka bet kokiam portui
@@ -21,12 +19,12 @@ public class TodoAppController {
     @Autowired
     private TodoAppRepository tar;
 
-    @GetMapping("/todo")
+    @GetMapping("/todoapps")
     public Iterable<TodoApp> getAllTodoApps() {
         return tar.findAll();
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/todo/{id}")
+    @RequestMapping(method = RequestMethod.GET, path = "/todoapps/{id}")
     public TodoApp getAllTodoApp(@PathVariable Integer id) {
         return tar.findById(id).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
@@ -35,7 +33,7 @@ public class TodoAppController {
     // add todoApp
     @RequestMapping(
             method = RequestMethod.POST,
-            path = "/todo",
+            path = "/todoapps",
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<String> addTodoApp(
@@ -44,14 +42,15 @@ public class TodoAppController {
         tar.save(postApp);
         return new ResponseEntity<>(HttpStatus.CREATED); // 201
     }
-    //            @RequestBody Map<String> params )
-//        tar.save(new TodoApp(params.get("cardText")));
-//    return new ResponseEntity<>(HttpStatus.CREATED);
 
     // delete todoApp
-    @RequestMapping(method = RequestMethod.DELETE, path="/todo/{id}")
-    public Map deleteTodoApp(@PathVariable String id, Map postsApp) {
-        postsApp.remove(id);
-        return postsApp;
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            path="/todoapps/{id}")
+
+    public ResponseEntity<?> deleteTodoAppById(@PathVariable("id") Integer id) {
+        tar.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+
     }
 }
--- src/main/java/com/example/springtodoapp/repository/TodoAppRepository.java
+++ src/main/java/com/example/springtodoapp/repository/TodoAppRepository.java
@@ -4,5 +4,10 @@ import com.example.springtodoapp.model.TodoApp;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+
 @Repository
-public interface TodoAppRepository extends CrudRepository<TodoApp, Integer> { }
+public interface TodoAppRepository extends CrudRepository<TodoApp, Integer> {
+
+    void deleteById(Integer id);
+}
